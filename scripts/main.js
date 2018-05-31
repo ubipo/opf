2a262f2991e884649228904b258f859b41f72f8a
Pieter Fiers <Ubipo.Skippy@gmail.com>
2018-06-01 00:40:43 +0200

    Dependencies fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,18 @@
 requirejs.config({
-	baseUrl: 'scripts/opf',
-    "paths": {
-      "leaflet": "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet",
-      "leaflet.markercluster": "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.3.0/leaflet.markercluster",
-      "leaflet.awesome-markers": "https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.min"
+  baseUrl: 'scripts/opf',
+  'paths': {
+    'leaflet': '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet',
+    'leaflet.markercluster': '//cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.3.0/leaflet.markercluster',
+    'leaflet.awesome-markers': '//cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.min'
+  },
+  shim: {
+    'leaflet.markercluster': {
+      deps: ['leaflet']
+    },
+    'leaflet.awesome-markers': {
+      deps: ['leaflet']
     }
+  }
 });
 
-requirejs(["scripts/opf/main.js"]);
+requirejs(['scripts/opf/main.js']);
